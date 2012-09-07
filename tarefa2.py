680bf18084054ff3a7616a0bdb8f267580ae98fc
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-09-07 19:59:50 -0300

    Most work done

--- tarefa2.py
+++ tarefa2.py
@@ -8,75 +8,94 @@ sample_file = open('sample', 'r')
 test_file = open('test', 'r')
 
 ## Parse sample data
-sample = []
+samples = []
 for line in sample_file:
   pair = line.split()
-  sample.append([float(pair[0]), int(pair[1])-1])
+  samples.append([float(pair[0]), int(pair[1])-1])
 sample_file.close()
 
 ## Parse test data
-test = []
+tests = []
 for line in test_file:
   pair = line.split()
-  test.append([float(pair[0]), int(pair[1])-1])
+  tests.append([float(pair[0]), int(pair[1])-1])
 test_file.close()
 
-## Group data by classes
-raw_classes = [[], [], []]
-for data in sample:
-  raw_classes[data[1]].append(data[0])
+## Group data by class samples
+raw_class_samples = [[], [], []]
+for data in samples:
+  raw_class_samples[data[1]].append(data[0])
 
-## Convert to numpy arrays
-classes = [array(c) for c in raw_classes]
+## Class to hold samples' relevant information
+class SampleInfo:
+  def __init__ (self, data):
+    self.min = data.min()
+    self.max = data.max()
+    self.mean = data.mean()
+    self.std = data.std()
 
-distributions = [ uniform, expon, norm ]
+## Convert to numpy arrays and extract relevant info
+class_samples = [SampleInfo(array(c)) for c in raw_class_samples]
 
+## Base likelihood probability class
 class Likelihood:
-  def __init__(self, dist, data):
-    if dist == uniform:
-      self.min = data.min()
-      self.max = data.max()
-      self.likelihood = lambda self,x: dist.pdf(x, self.min, self.max)
-    elif dist == expon:
-      self.lamb = 1.0/data.mean()
-      self.likelihood = lambda self,x: dist.pdf(x, 0, self.lamb)
-    elif dist == norm:
-      self.mean = data.mean()
-      self.std = data.std()
-      self.likelihood = lambda self,x: dist.pdf(x, self.mean, self.std)
+  def __init__(self, dist, sample):
+    self.dist = dist
+    self.sample = sample
   def __call__(self, x):
-    return self.likelihood(self,x)
+    return self.dist.pdf(x, self.local(), self.scale())
+
+## Uniform distribution likelihood class
+class UniformLikelihood(Likelihood):
+  def local(self):
+    return self.sample.min
+  def scale(self):
+    return self.sample.max - self.sample.min
+
+## Exponencial distribution likelihood class
+class ExponencialLikelihood(Likelihood):
+  def local(self):
+    return 0
+  def scale(self):
+    return self.sample.mean ## 1.0/lambda
+
+## Normal distribution likelihood class
+class NormalLikelihood(Likelihood):
+  def local(self):
+    return self.sample.mean
+  def scale(self):
+    return self.sample.std
+
+likelihood_map = {
+  uniform: UniformLikelihood,
+  expon: ExponencialLikelihood,
+  norm: NormalLikelihood
+}
 
 class Classifier:
-  def __init__(self, dist1, dist2, dist3):
-    self.likelihoods = [ Likelihood(dist1, classes[0]),
-                         Likelihood(dist2, classes[1]),
-                         Likelihood(dist3, classes[2]) ]
+  def __init__(self, dists):
+    self.likelihoods = [
+      likelihood_map[dist] (dist, class_samples[i]) for i,dist in enumerate(dists)
+    ]
   def __call__(self,x):
-    argmax = 0
-    posteriori = 0
-    for i in range(3):
-      p = self.likelihoods[i](x)
-      if p > posteriori:
-        posteriori = p
-        argmax = i
-    return argmax
+    ## reminder: P(class) = 1/3 always
+    posteriors = array([ likely(x)*(1.0/3.0) for likely in self.likelihoods ])
+    return posteriors.argmax()
 
+distributions = {
+  'U': uniform,
+  'E': expon,
+  'N': norm
+}
 
-for dist1 in distributions:
-  for dist2 in distributions:
-    for dist3 in distributions:
-      classify = Classifier(dist1, dist2, dist3)
+for k1,dist1 in distributions.iteritems():
+  for k2,dist2 in distributions.iteritems():
+    for k3,dist3 in distributions.iteritems():
+      classify = Classifier([dist1, dist2, dist3])
       hits = 0
-      for x in test:
+      for x in tests:
         if classify(x[0]) == x[1]:
           hits += 1
-      print(dist1, dist2, dist3, hits)
+      print(k1, k2, k3, hits)
 
-
-##print [c.min() for c in classes]
-##print [c.max() for c in classes]
-##print [c.mean() for c in classes]
-##print [c.var() for c in classes]
-##print norm.pdf(0.000161578142981)
 
